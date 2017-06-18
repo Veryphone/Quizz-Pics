3aab9334ec267e7115776f0844c9ff9e7f91b8af
Veryphone <veryphone@gmail.com>
2017-06-18 21:16:56 +0200

    0001

---
+++ Assets/Pics Quiz Maker With Categories/CategorySelection/Scripts/RefreshQuizSelectionLevels.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RefreshQuizSelectionLevels : MonoBehaviour
+{
+
+    public static string selectedCategory = "";
+    public static int levelSelected = -1;
+    public GameObject levelImagePrefab;
+    Word_Database Words_Database_Selected;
+    int solvedWords;
+    int totalWords;
+    public void selectedThisGameObjectName ()
+    {
+        selectedCategory = gameObject.name;
+        PlayerPrefs.SetString("SelectedCategory", selectedCategory);
+    }
+    void Start ()
+    {
+
+        if (GameObject.Find("Main Camera").transform.position.x == -11 && PlayerPrefs.HasKey("SelectedCategory") && gameObject.name == "Quiz_Selection_Controller")
+        {
+            refreshQuizLevels();
+        }
+        
+    }
+    public void refreshQuizLevels()
+    {
+        selectedCategory = PlayerPrefs.GetString("SelectedCategory");
+        foreach (Transform transformObj in GameObject.Find("QUIZ_SELECTION").transform.FindChild("Canvas").transform.FindChild("Mask").transform.FindChild("ScrollRect").transform.FindChild("Levels_Container").transform)
+        {
+            DestroyImmediate(transformObj.gameObject);
+        }
+
+        Words_Database_Selected = GameObject.Find("DATABASES").transform.FindChild(selectedCategory).GetComponent<Word_Database>();
+
+        GameObject.Find("QUIZ_SELECTION").transform.FindChild("Canvas").transform.FindChild("TopBox").transform.FindChild("Title").GetComponent<Text>().text = Words_Database_Selected.nameOfCategory[PlayerPrefs.GetInt("numberLanguae")];
+
+        for (int i = 0; i < Words_Database_Selected.words_List.Length;)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (i < Words_Database_Selected.words_List.Length)
+                {
+                    GameObject go = Instantiate(GameObject.Find("Quiz_Selection_Controller").GetComponent<RefreshQuizSelectionLevels>().levelImagePrefab, new Vector3(-11, 0, 0), Quaternion.identity) as GameObject;
+                    go.transform.SetParent(GameObject.Find("QUIZ_SELECTION").transform.FindChild("Canvas").transform.FindChild("Mask").transform.FindChild("ScrollRect").transform.FindChild("Levels_Container").transform);
+                    go.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                    go.transform.FindChild("Image").GetComponent<Image>().sprite = Words_Database_Selected.image[i];
+                    if (Words_Database_Selected.solvedWords[i])
+                    {
+                        go.transform.FindChild("SolvedQuiz").gameObject.SetActive(true);
+                        go.GetComponent<Image>().color = Color.green;
+                        solvedWords++;
+                    }
+                    totalWords++;
+                    go.name = i.ToString();
+                    i++;
+                }
+            }
+        }
+
+        GameObject.Find("Main Camera").transform.position = new Vector3(-11, 0, -10);
+
+        GameObject.Find("QUIZ_SELECTION").transform.FindChild("Canvas").transform.FindChild("TopBox").transform.FindChild("SolvedWords").transform.FindChild("TotalSolvedWords").GetComponent<Text>().text = solvedWords + "/" + totalWords;
+        GameObject.Find("QUIZ_SELECTION").transform.FindChild("Canvas").transform.FindChild("TopBox").transform.FindChild("TotalPorcent").transform.FindChild("TotalPorcent").GetComponent<Text>().text = ((solvedWords * 100) / totalWords).ToString() + "%";
+
+    }
+
+    public void setLevelSelected()
+    {
+          levelSelected = int.Parse(gameObject.name);
+          Application.LoadLevel(Application.loadedLevelName);
+          GameObject.Find("Main Camera").GetComponent<Animation>().Play("startScene");
+          GameObject.Find("Main Camera").transform.position = new Vector3(0, 0, -10);
+     
+    }
+}
---
+++ Assets/Pics Quiz Maker With Categories/CategorySelection/Scripts/SelectCategory.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectCategory : MonoBehaviour {
+
+    [SerializeField] GameObject categoryPrefab;
+    int totalSolvedLevels;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        GameObject databaseGO = GameObject.Find("DATABASES");
+        foreach (Transform child in databaseGO.transform)
+        {
+            GameObject go = Instantiate(categoryPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+            //change the text of "Solved questions" for the translation
+            go.transform.FindChild("QuestionsSolved").GetComponent<Text>().text = child.GetComponent<Word_Database>().uiTextsLang[PlayerPrefs.GetInt("numberLanguae"), 17];
+            go.transform.SetParent(GameObject.Find("CATEGORY_SELECTION").transform.FindChild("Canvas").transform.FindChild("Mask").transform.FindChild("ScrollRect").transform.FindChild("Levels_Container").transform);
+            go.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            go.transform.FindChild("NameLevel").GetComponent<Text>().text = child.GetComponent<Word_Database>().nameOfCategory[PlayerPrefs.GetInt("numberLanguae")];
+            
+            for (int i = 0; i < child.GetComponent<Word_Database>().solvedWords.Length; i++)
+            {
+                if (child.GetComponent<Word_Database>().solvedWords[i])
+                {
+                    totalSolvedLevels++;
+                }
+            }
+
+         
+            go.transform.FindChild("NumberSolved").GetComponent<Text>().text = totalSolvedLevels + "/"+child.GetComponent<Word_Database>().words_List.Length;
+            float porcent = (totalSolvedLevels * 100) / child.GetComponent<Word_Database>().words_List.Length;
+            go.transform.FindChild("PorcentBar").transform.FindChild("Porcent").GetComponent<Text>().text = porcent + "%";
+            go.transform.FindChild("PorcentBar").transform.FindChild("WhiteBar").GetComponent<RectTransform>().localPosition = new Vector3(563 * (porcent/100), 0, 0);
+            go.name = child.name;
+            if (child.GetComponent<Word_Database>().howManyQuizSolvedNeedToUnlock > CategoryControlOther.totalSolvedWords)
+            {
+                go.GetComponent<Image>().color = new Vector4 (0, 0.5f, 0.65f, 1);
+                go.transform.FindChild("PorcentBar").transform.FindChild("Porcent").GetComponent<Text>().text = child.GetComponent<Word_Database>().uiTextsLang[PlayerPrefs.GetInt("numberLanguae"), 18] + child.GetComponent<Word_Database>().howManyQuizSolvedNeedToUnlock+ child.GetComponent<Word_Database>().uiTextsLang[PlayerPrefs.GetInt("numberLanguae"), 19];
+                Destroy(go.GetComponent<Button>());
+                go.transform.FindChild("Locked").gameObject.SetActive(true);
+                Destroy(go.transform.FindChild("PorcentBar").GetComponent<Mask>());
+                Destroy(go.transform.FindChild("PorcentBar").GetComponent<Image>());
+                Destroy(go.transform.FindChild("PorcentBar").transform.FindChild("WhiteBar").GetComponent<Image>());
+
+            }
+
+            totalSolvedLevels = 0;
+        }
+	}
+
+}
